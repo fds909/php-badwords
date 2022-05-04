ac6c6fa0902b578e5f90ec9240af71671cd43459
Francesco De Sìmini <francescod995@gmail.com>
2022-05-04 15:53:58 +0200

    fatto

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bad Words</title>
+</head>
+<body>
+  <?php
+    $paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis imperdiet placerat urna, non lacinia mauris ornare nec. Morbi ut sem in neque ultricies tristique ut sed sem.";
+    echo ("<p> $paragraph" . "[lunghezza: " . strlen($paragraph) . "]</p>");
+    $paragraph_censored = str_replace($_GET['badword'], '***', $paragraph);
+    echo ("<p>Testo censurato: " . $paragraph_censored ."</p>");
+  ?>
+
+  <form method="GET" action="index.php">
+    <label>Paorola da censurare: </label>
+    <input type="text" name="badword"/>
+    <input type="submit"/>
+  </form>
+</body>
+</html>
